e82508879a1a1e43fa0c577d4dcaab95f201c9cf
Yannick Schutz <hello@yannickschutz.com>
2022-07-28 19:35:32 +0000

    Move to config. ve

---
+++ indiekit.config.cjs
@@ -0,0 +1,44 @@
+const process = require("node:process");
+require("dotenv").config();
+
+module.exports = {
+  application: {
+    mongodbUrl: process.env.ORMONGO_URL
+  },
+  plugins: [
+    "@indiekit/preset-hugo",
+    "@indiekit/store-github"
+  ],
+  publication: {
+    me: "https://yannickschutz.com",
+    postTypes: [
+      {
+        type: "note",
+        name: "Journal entry",
+        post: {
+          path: "journal/{yyyy}-{MM}-{dd}-{slug}.md",
+          url: "journal/{yyyy}/{MM}/{slug}"
+        }
+      },
+      {
+        type: "photo",
+        name: "Photograph",
+        post: {
+          path: "journal/{yyyy}-{MM}-{dd}-{slug}.md",
+          url: "journal/{yyyy}/{MM}/{slug}"
+        },
+        media: {
+          path: "media/photos/{yyyy}/{filename}"
+        }
+      }
+    ]
+  },
+  "@indiekit/preset-hugo": {
+    frontMatterFormat: "yaml"
+  },
+  "@indiekit/store-github": {
+    user: "ys",
+    repo: "bonjour",
+    branch: "main"
+  }
+};
